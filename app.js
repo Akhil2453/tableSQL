f45f820081705bcdcd1e42539556e4e6210b4589
Akhil2453 <akhil.menon94@gmail.com>
2018-03-23 13:40:01 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 var sql = require('mysql');
-var http = require('http');
+
 var express = require('express');
 var app = express();
 var bParser = require('body-parser');
 var fs = require('fs');
-
-http.createServer(function(req, res){
-	fs.readFile('index.html', function(err, data){
-		res.writeHead(200, {'Content-Type' : 'text/html'});
-		res.write(data);
-		res.end();
-	});
-}).listen(8080);
-
-
 
 var conn = sql.createConnection({
 	host : 'localhost',
@@ -27,15 +17,15 @@ conn.connect(function(err){
 	console.log("Connected!");
 });
 
-// app.get("/", function(req, res){
-// 	conn.query('SELECT * FROM assign LIMIT 2', function(err, rows, fields){
-// 		conn.end();
-// 		if(!err){
-// 			console.log('The solution is: ', rows);
-// 		} else {
-// 			console.log('Error while performing query.');
-// 		}
-// 	});
-// });
+app.get("/", function(req, res){
+	conn.query('SELECT * FROM assign LIMIT 2', function(err, rows, fields){
+		conn.end();
+		if(!err){
+			console.log('The solution is: ', rows);
+		} else {
+			console.log('Error while performing query.');
+		}
+	});
+});
 
-// app.listen(3000);
+app.listen(3000);
